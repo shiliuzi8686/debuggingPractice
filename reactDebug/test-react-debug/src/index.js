fbbb48716ac5eb490ab87ed8f2eb39de6b5fcb01
yuting2.liu <yuting2.liu@ly.com>
2024-10-28 19:00:53 +0800

    feat(vueDebugger): vueDebugger

--- reactDebug/test-react-debug/src/index.js
+++ reactDebug/test-react-debug/src/index.js
@@ -5,6 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log('React.StrictMode')
+console.log(222);
+console.log(333);
+console.log(444);
+
 root.render(
   <React.StrictMode>
     <App />
