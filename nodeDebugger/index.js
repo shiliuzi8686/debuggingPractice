7a2b85384263f1692cdb4c134b3c72b542ec9cca
yuting2.liu <yuting2.liu@ly.com>
2024-10-31 19:11:48 +0800

    feat: nodedebugger

---
+++ nodeDebugger/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs/promises');
+
+(async function() {
+  const fileContent = await fs.readFile('./package.json', {
+    encoding: 'utf-8',
+  })
+  await fs.writeFile('./package2.json', fileContent, {
+    encoding: 'utf-8',
+  })
+})()
